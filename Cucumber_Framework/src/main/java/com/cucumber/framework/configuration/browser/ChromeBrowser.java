7a520b630211d7c8cecccea93ad8e3c3f9beebcf
499449 <499449@PC227921.cts.com>
2018-03-20 21:46:19 +0530

    2nd

--- Cucumber_Framework/src/main/java/com/cucumber/framework/configuration/browser/ChromeBrowser.java
+++ Cucumber_Framework/src/main/java/com/cucumber/framework/configuration/browser/ChromeBrowser.java
@@ -3,17 +3,12 @@ package com.cucumber.framework.configuration.browser;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class ChromeBrowser {
 	public WebDriver getChromeDriver() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\drivers\\chromedriver.exe");
 		ChromeOptions option = new ChromeOptions();
 		option.addArguments("start-maximized");
-		DesiredCapabilities chrome = DesiredCapabilities.chrome();
-		chrome.setJavascriptEnabled(true);
-		chrome.setCapability(ChromeOptions.CAPABILITY, option);
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\drivers\\chromedriver.exe");
-		return new ChromeDriver(chrome);
+		return new ChromeDriver(option);
 	}
-
 }
